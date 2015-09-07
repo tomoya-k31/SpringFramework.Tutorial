b754ae78ff5d3637234b99ccd726e4dec1976080
tomoya-k31 <chacogre@gmail.com>
2015-09-08 02:52:27 +0900

    Part2

---
+++ SpringBoot.Reference1/src/main/java/me/tomoya/MyApplication.java
@@ -0,0 +1,23 @@
+package me.tomoya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by usr0200379 on 15/09/08.
+ */
+@RestController
+@EnableAutoConfiguration
+public class MyApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
